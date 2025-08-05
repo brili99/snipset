5455816576ff1a2a991dc0c43b9eb16d8486f74f
Muhammad Brilian Erranaomi <49330514+brili99@users.noreply.github.com>
2025-08-05 14:59:24 +0700

    Create Env.php

---
+++ php/Env.php
@@ -0,0 +1,9 @@
+<?php
+const DEBUG = true;
+const DB_HOST = 'localhost';
+const DB_USER = 'user';
+const DB_PASS = 'pass';
+const DB_NAME = 'database';
+const DB_PORT = 3306;
+const PATH_ROOT = __DIR__ . '/../';
+const PATH_IMG_STORAGE = PATH_ROOT . '/api/storage/';
