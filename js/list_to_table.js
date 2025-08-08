9615e0154189edde6c87ddf35d36f89acaaa3ef7
Muhammad Brilian Erranaomi <49330514+brili99@users.noreply.github.com>
2025-08-08 09:07:09 +0700

    Update list_to_table.js

--- js/list_to_table.js
+++ js/list_to_table.js
@@ -2,6 +2,19 @@ $(function () {
     const thPricingTable = $("#pricingTable > thead");
     const tbPricingTable = $("#pricingTable > tbody");
 
+    function update_kamar_data(roomType, field, value) {
+        $.post("/api/web/kamar.php", {
+            action: "update_kamar_data",
+            token: localStorage.getItem('token'),
+            roomType, field, value
+        }).done(res => {
+            if (res.status == false) {
+                alert(res.msg || "Failed to update data");
+                return;
+            }
+        });
+    }
+
     function get_list_tipe_kamar() {
         $.get("/api/web/kamar.php", { action: "list_tipe_kamar" })
             .fail(failHandler)
@@ -16,9 +29,14 @@ $(function () {
                 }
                 const data = res.data;
                 const head = Object.keys(data[0]);
+                const primaryKey = "roomType";
                 console.log(data);
                 thPricingTable.html(be("tr", {}, head.map(h => be("th", {}, [h.replaceAll('_', " ")]))));
-                tbPricingTable.html(data.map(d => be("tr", {}, head.map(h => be("td", {}, [d[h]])))));
+                tbPricingTable.html(data.map(d => be("tr", {}, head.map(h => be("td", {
+                    contenteditable: h != primaryKey ? "true" : "false", title: "Click to edit"
+                }, [d[h]], {
+                    focusout: (e) => update_kamar_data(d[primaryKey], h, e.target.innerText)
+                })))));
             });
     }
     get_list_tipe_kamar();
